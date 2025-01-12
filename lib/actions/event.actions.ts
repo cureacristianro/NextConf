90ce71bc3b265190b62c207480176127f5d052a5
cureacristianro <cristian.curea03@e-uvt.ro>
2025-01-12 23:37:05 +0200

    Fix to try to create multiple users and create events

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,12 +1,12 @@
-'use server';
+"use server";
 
-import { revalidatePath } from 'next/cache';
+import { revalidatePath } from "next/cache";
 
-import { connectToDatabase } from '@/lib/database';
-import Event from '@/lib/database/models/event.model';
-import User from '@/lib/database/models/user.model';
-import Category from '@/lib/database/models/category.model';
-import { handleError } from '@/lib/utils';
+import { connectToDatabase } from "@/lib/database";
+import Event from "@/lib/database/models/event.model";
+import User from "@/lib/database/models/user.model";
+import Category from "@/lib/database/models/category.model";
+import { handleError } from "@/lib/utils";
 
 import {
   CreateEventParams,
@@ -15,114 +15,131 @@ import {
   GetAllEventsParams,
   GetEventsByUserParams,
   GetRelatedEventsByCategoryParams,
-} from '@/types';
+} from "@/types";
 
 const getCategoryByName = async (name: string) => {
-  console.log('Searching for category:', name);
-  return Category.findOne({ name: { $regex: name, $options: 'i' } });
+  console.log("Searching for category:", name);
+  return Category.findOne({ name: { $regex: name, $options: "i" } });
 };
 
 const populateEvent = (query: any) => {
   return query
-      .populate({ path: 'organizer', model: User, select: '_id firstName lastName' })
-      .populate({ path: 'category', model: Category, select: '_id name' });
+    .populate({
+      path: "organizer",
+      model: User,
+      select: "_id firstName lastName",
+    })
+    .populate({ path: "category", model: Category, select: "_id name" });
 };
 
 // CREATE
 export async function createEvent({ userId, event, path }: CreateEventParams) {
   try {
     await connectToDatabase();
-    console.log('Received userId:', userId);
+    console.log("Received userId:", userId);
 
-    const organizer = await User.findById(userId);
+    const organizer = await User.findOne({ userId });
     if (!organizer) {
-      console.error('Organizer not found for ID:', userId);
-      throw new Error('Organizer not found');
+      console.error("Organizer not found for ID:", userId);
+      throw new Error("Organizer not found");
     }
 
-    const newEvent = await Event.create({ ...event, category: event.categoryId, organizer: userId });
-    console.log('Event created successfully:', newEvent);
+    const newEvent = await Event.create({
+      ...event,
+      category: event.categoryId,
+      organizer: userId,
+    });
+    console.log("Event created successfully:", newEvent);
 
     revalidatePath(path);
     return JSON.parse(JSON.stringify(newEvent));
   } catch (error: unknown) {
     if (error instanceof Error) {
-      console.error('Error in createEvent:', error.message);
+      console.error("Error in createEvent:", error.message);
     } else {
-      console.error('Unknown error in createEvent:', error);
+      console.error("Unknown error in createEvent:", error);
     }
     handleError(error);
   }
 }
-
 
 // UPDATE
 export async function updateEvent({ userId, event, path }: UpdateEventParams) {
   try {
     await connectToDatabase();
-    console.log('Updating event for userId:', userId);
+    console.log("Updating event for userId:", userId);
 
     const eventToUpdate = await Event.findById(event._id);
     if (!eventToUpdate || eventToUpdate.organizer.toHexString() !== userId) {
-      console.error('Unauthorized or event not found');
-      throw new Error('Unauthorized or event not found');
+      console.error("Unauthorized or event not found");
+      throw new Error("Unauthorized or event not found");
     }
 
     const updatedEvent = await Event.findByIdAndUpdate(
-        event._id,
-        { ...event, category: event.categoryId },
-        { new: true }
+      event._id,
+      { ...event, category: event.categoryId },
+      { new: true }
     );
-    console.log('Event updated successfully:', updatedEvent);
+    console.log("Event updated successfully:", updatedEvent);
 
     revalidatePath(path);
     return JSON.parse(JSON.stringify(updatedEvent));
   } catch (error: unknown) {
     if (error instanceof Error) {
-      console.error('Error in updateEvent:', error.message);
+      console.error("Error in updateEvent:", error.message);
     } else {
-      console.error('Unknown error in updateEvent:', error);
+      console.error("Unknown error in updateEvent:", error);
     }
     handleError(error);
   }
 }
-
 
 // DELETE
 export async function deleteEvent({ eventId, path }: DeleteEventParams) {
   try {
     await connectToDatabase();
-    console.log('Deleting event with ID:', eventId);
+    console.log("Deleting event with ID:", eventId);
 
     const deletedEvent = await Event.findByIdAndDelete(eventId);
     if (deletedEvent) revalidatePath(path);
   } catch (error: unknown) {
     if (error instanceof Error) {
-      console.error('Error in deleteEvent:', error.message);
+      console.error("Error in deleteEvent:", error.message);
     } else {
-      console.error('Unknown error in deleteEvent:', error);
+      console.error("Unknown error in deleteEvent:", error);
     }
     handleError(error);
   }
 }
 
-
 // GET ALL EVENTS
-export async function getAllEvents({ query, limit = 6, page, category }: GetAllEventsParams) {
+export async function getAllEvents({
+  query,
+  limit = 6,
+  page,
+  category,
+}: GetAllEventsParams) {
   try {
     await connectToDatabase();
 
-    const titleCondition = query ? { title: { $regex: query, $options: 'i' } } : {};
-    const categoryCondition = category ? await getCategoryByName(category) : null;
+    const titleCondition = query
+      ? { title: { $regex: query, $options: "i" } }
+      : {};
+    const categoryCondition = category
+      ? await getCategoryByName(category)
+      : null;
     const conditions = {
-      $and: [titleCondition, categoryCondition ? { category: categoryCondition._id } : {}],
+      $and: [
+        titleCondition,
+        categoryCondition ? { category: categoryCondition._id } : {},
+      ],
     };
 
     const skipAmount = (Number(page) - 1) * limit;
     const eventsQuery = Event.find(conditions)
-        .sort({ createdAt: 'desc' })
-        .skip(skipAmount)
-        .limit(limit);
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(limit);
 
     const events = await populateEvent(eventsQuery);
     const eventsCount = await Event.countDocuments(conditions);
@@ -133,34 +150,40 @@ export async function getAllEvents({ query, limit = 6, page, category }: GetAllE
     };
   } catch (error: unknown) {
     if (error instanceof Error) {
-      console.error('Error in getAllEvents:', error.message);
+      console.error("Error in getAllEvents:", error.message);
     } else {
-      console.error('Unknown error in getAllEvents:', error);
+      console.error("Unknown error in getAllEvents:", error);
     }
     handleError(error);
   }
 }
 
-
 // GET EVENTS BY ORGANIZER
-export async function getEventsByUser({ userId, limit = 6, page }: GetEventsByUserParams) {
+export async function getEventsByUser({
+  userId,
+  limit = 6,
+  page,
+}: GetEventsByUserParams) {
   try {
-    await connectToDatabase()
+    await connectToDatabase();
 
-    const conditions = { organizer: userId }
-    const skipAmount = (page - 1) * limit
+    const conditions = { organizer: userId };
+    const skipAmount = (page - 1) * limit;
 
     const eventsQuery = Event.find(conditions)
-      .sort({ createdAt: 'desc' })
+      .sort({ createdAt: "desc" })
       .skip(skipAmount)
-      .limit(limit)
+      .limit(limit);
 
-    const events = await populateEvent(eventsQuery)
-    const eventsCount = await Event.countDocuments(conditions)
+    const events = await populateEvent(eventsQuery);
+    const eventsCount = await Event.countDocuments(conditions);
 
-    return { data: JSON.parse(JSON.stringify(events)), totalPages: Math.ceil(eventsCount / limit) }
+    return {
+      data: JSON.parse(JSON.stringify(events)),
+      totalPages: Math.ceil(eventsCount / limit),
+    };
   } catch (error) {
-    handleError(error)
+    handleError(error);
   }
 }
 
@@ -172,22 +195,27 @@ export async function getRelatedEventsByCategory({
   page = 1,
 }: GetRelatedEventsByCategoryParams) {
   try {
-    await connectToDatabase()
+    await connectToDatabase();
 
-    const skipAmount = (Number(page) - 1) * limit
-    const conditions = { $and: [{ category: categoryId }, { _id: { $ne: eventId } }] }
+    const skipAmount = (Number(page) - 1) * limit;
+    const conditions = {
+      $and: [{ category: categoryId }, { _id: { $ne: eventId } }],
+    };
 
     const eventsQuery = Event.find(conditions)
-      .sort({ createdAt: 'desc' })
+      .sort({ createdAt: "desc" })
       .skip(skipAmount)
-      .limit(limit)
+      .limit(limit);
 
-    const events = await populateEvent(eventsQuery)
-    const eventsCount = await Event.countDocuments(conditions)
+    const events = await populateEvent(eventsQuery);
+    const eventsCount = await Event.countDocuments(conditions);
 
-    return { data: JSON.parse(JSON.stringify(events)), totalPages: Math.ceil(eventsCount / limit) }
+    return {
+      data: JSON.parse(JSON.stringify(events)),
+      totalPages: Math.ceil(eventsCount / limit),
+    };
   } catch (error) {
-    handleError(error)
+    handleError(error);
   }
 }
 
@@ -199,9 +227,9 @@ export async function getEventById(eventId: string) {
     return JSON.parse(JSON.stringify(event));
   } catch (error: unknown) {
     if (error instanceof Error) {
-      console.error('Error in getEventById:', error.message);
+      console.error("Error in getEventById:", error.message);
     } else {
-      console.error('Unknown error in getEventById:', error);
+      console.error("Unknown error in getEventById:", error);
     }
     handleError(error);
   }
