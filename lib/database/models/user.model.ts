d71c416f0628bb4ce4e574da3a403f0e1c45c250
cureacristianro <cristian.curea03@e-uvt.ro>
2025-01-13 10:21:04 +0200

    Correct user schema

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,7 +1,6 @@
 import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
-  _id: { type: String, required: true, unique: true },
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   firstName: { type: String, required: false },
